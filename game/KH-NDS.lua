eebdb363871e8e6ba66dba148e1ef75c5bd39aed
Kater <91840948+Colin4President@users.noreply.github.com>
2024-10-04 15:14:02 +0200

    Update KH-NDS.lua

--- game/KH-NDS.lua
+++ game/KH-NDS.lua
@@ -1,1 +1,1 @@
-print("hi")
+game:GetService("StarterGui"):SetCore("SendNotification", {Title = "KATERHUB";Text = "Currently in development, v1 will launch soon!";Duration = 45;Button1 = "Continue";})
