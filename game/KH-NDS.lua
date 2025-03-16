df097648b0ffcdb0e8cf52fd98b611b776345896
Colin <91840948+Colin4President@users.noreply.github.com>
2025-03-16 13:06:01 +0100

    Update KH-NDS.lua

--- game/KH-NDS.lua
+++ game/KH-NDS.lua
@@ -1,2 +1,18 @@
 -- This is a placeholder for the old loadstring
+
+if setclipboard then
+  setclipboard('loadstring(game:HttpGet("https://raw.githubusercontent.com/KaterHub-Inc/NaturalDisasterSurvival/refs/heads/main/main.lua"))()')
+  game:GetService("StarterGui"):SetCore("SendNotification", {
+      Title = "NDS Hub";
+      Text = "Please update your loadstring!!\n\nThe new one has been copied to your clipboard.";
+      Duration = 60;
+  })
+else
+  game:GetService("StarterGui"):SetCore("SendNotification", {
+      Title = "NDS Hub";
+      Text = "Please update your loadstring!!";
+      Duration = 60;
+  })
+end
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/KaterHub-Inc/NaturalDisasterSurvival/refs/heads/main/main.lua"))()
